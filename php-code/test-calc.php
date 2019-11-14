d43145e7a7018952c00ae5d35af138cd5ee3b593
useralexey2000 <useralexey2000@gmail.com>
2019-11-14 19:56:49 +0200

    modifying validation & default select field(cause of index err)

--- php-code/test-calc.php
+++ php-code/test-calc.php
@@ -1,15 +1,12 @@
 <head>
 <style>
-        .main_tbl {
+        .main-tbl {
             width: 100%;
             border-collapse: collapse;
         }
-        .main_tbl td, .main_tbl th {
+        .main-tbl td, .main-tbl th {
             border: 1px solid #dddd;
             padding : 5px;
-        }
-        .main_tbl .num_in {
-            padding: 5px;
         }
         .disabled {
             width: 20%;
@@ -24,61 +21,72 @@
 <body>
     
 <form method="post" action="<?php htmlspecialchars($_SERVER['PHP_SELF']);?>">
-<table class = "main_tbl">
+<table class = "main-tbl">
     <thead>
         <tr><th>one</th><th>two</th><th>three</th><th>four</th></tr>
     </thead>
     <tbody>
 <?php
-    $rows = 5;
-    $cols = 4;
-    $arr = array_fill(0, $rows*$cols, 0);
-    $err_messg = "";
-    $res_err = "disabled";
-    $data_err = "";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-    function fields_validate($data){
-        $data = htmlspecialchars($data);
-        if (is_numeric($data)){
-            return true;
-        }
-        return false;
-    }
 
+$rows = 4;
+$cols = 5;
+$arr = array_fill(0, $rows*$cols, 0);
+$err_messg = "";
+$res_err = "disabled";
+$data_err = "";
+
+function fields_validate($data){
+    $data = htmlspecialchars($data);
+    $data = trim($data);
+    $data = stripslashes($data);
+    return $data;
+}
+
+if(isset($_POST['compute'])){
     
-    
-    if(isset($_POST['compute'])){
+    for ($i=0; $i <$rows*$cols; $i++) { 
+        $myin = fields_validate($_POST['myin'.$i]);
+        $math_act = fields_validate($_POST['math-act'.$i]);
+        $res = fields_validate($_POST['res'.$i]);
         
-        for ($i=0; $i <$rows*$cols; $i++) { 
-            $myin = $_POST['myin'.$i];
-            $math_act = $_POST['math_act'.$i];
-            $res = $_POST['res'.$i];
-            
-            if(!fields_validate($myin)){
-                $myin = 0;
-                $err_messg = "not valid data enter correct data";
-
-            }
-            if(!fields_validate($res)){
-                $err_messg = "not valid data check fields";
-                $arr[$i] = 0;
-            }
-            
-            switch ($math_act) {
-                case 'add':
-                    $arr[$i] = $res + $myin;
+        if(!is_numeric($myin)){
+            $myin = 0;
+            $arr[$i] = $res;
+            $err_messg = "not valid data enter correct data";
+            continue;
+        }
+        if(!is_numeric($res)){
+            $err_messg = "not valid data enter correct data";
+            $arr[$i] = 0;
+            continue;
+        }
+        
+        switch ($math_act) {
+            case 'add':
+                $arr[$i] = $res + $myin;
+            break;
+            case 'sub':
+                $arr[$i] = $res - $myin;
+            break;
+            case 'mul':
+                $arr[$i] = $res * $myin;
+            break;
+            case 'div':
+                if($myin == 0) {
+                    $err_messg = "can not devide by 0";
                 break;
-                case 'sub':
-                    $arr[$i] = $res - $myin;
-                break;
-                case 'mul':
-                    $arr[$i] = $res * $myin;
-                break;
-                case 'div':
-                    $arr[$i] = $res / $myin;
-                break;
-                default:
-                $arr[$i] = $res;
+                }
+                $arr[$i] = $res / $myin;
+            break;
+            case 'miss':
+                $arr[$i] = $res;    
+            break;
+            default:
+                $err_messg = "not valid action check fields"; 
             break;
         }
     }
@@ -96,25 +104,24 @@ for($i = 0; $i < $rows; $i++) {
         echo '
         <td>
         <input class="disabled" type="text" name="res'.$counter.'" value="'.$arr[$counter].'">
-        <select class="math_act" name="math_act'.$counter.'">
-        <option selected disabled>action</option>
+        <select class="math-act" name="math-act'.$counter.'">
+            <option selected value="miss">action</option>
             <option value="add">+</option>
             <option value="sub">-</option>
             <option value="mul">*</option>
             <option value="div">/</option>
-            </select>
-            <input type="number" placeholder="0" step="0.00001" name="myin'.$counter.'" value="0">
-            </td>';
-            $counter++;
+        </select>
+        <input type="number" placeholder="0" step="0.00001" name="myin'.$counter.'" value="0">
+        </td>';
+
+        $counter++;
     }
-            echo "</tr>";
+    echo "</tr>";
 }
 ?>
-
 </tbody>
 </table>
 <input type="submit" name="compute" value="compute">
 <input type="submit" name="reset" value="reset">
 </form>
 </body>
-
